d9815d93ca1df85123e05cb094596e7c92b04206
Sefer Mirza <sefermirza@gmail.com>
2023-03-26 20:40:31 +0300

    add helloworld & variables

---
+++ beginning/veriables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // Variables
+  string text = "string";
+  int number = 5;
+  float length = 1.75;
+  char score = 'A';
+
+  cout << "Variables" << endl;
+  cout << "text: " << text << endl;
+  cout << "number: " << number << endl;
+  cout << "length: " << length << endl;
+  cout << "score: " << score << endl;
+
+  cout << endl;
+  cout << "Operators" << endl;
+  cout << "5 + 5: " << 5 + 5 << endl;
+  cout << "5 * 5: " << 5 * 5 << endl;
+  cout << "5 / 5:" << 5 / 5 << endl;
+  cout << "5 % 5:" << 5 % 5 << endl;
+}
