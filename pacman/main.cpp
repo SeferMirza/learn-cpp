a9797771a8f805c3a129d798f50a11b21146c70b
Sefer Mirza <sefermirza@gmail.com>
2023-12-26 23:05:00 +0300

    draw first board

--- pacman/main.cpp
+++ pacman/main.cpp
@@ -1,26 +1,59 @@
 #include <SFML/Graphics.hpp>
+#include <cassert>
+#include <iostream>
+
+const char *map = R""""(
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+X   Y    XXXX     Y       XXXX   Y    X
+X XXXXXX XXXX XXXXXXXXXXX XXXX XXXXXX X
+X XXXXXX XXXX XXXXXXXXXXX XXXX XXXXXX X
+X                                     X
+XXXXXXXX XXXX XXXXXDXXXXX XXXX XXXXXXXX
+XXXXXXXX XXXX X         X XXXX XXXXXXXX
+XXXXXXXX XXXX XXXXXXXXXXX XXXX XXXXXXXX
+X                                     X
+X XXXXXX XXXX XXXXXXXXXXX XXXX XXXXXX X
+X XXXXXX XXXX XXXXXXXXXXX XXXX XXXXXX X
+X   Y    XXXX      Y      XXXX   Y    X
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+)"""";
+
+sf::RectangleShape Wall(int x, int y)
+{
+    sf::RectangleShape rectangle(sf::Vector2f(20.f, 20.f));
+    rectangle.setFillColor(sf::Color::Blue);
+    rectangle.setPosition(index * 30.f, 0.f);
+
+    return rectangle;
+}
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
-    window.setFramerateLimit(60);
+    for (char c = *map; c; c=*++map) {
+        std::cout << c;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pacman");
 
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::End))
                 window.close();
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::End))
+        window.clear();
+        for (size_t i = 0; i < 40; i++)
         {
-            // left key is pressed: move our character
-            window.close();
+            window.draw(Wall(i));
         }
+
+        window.display();
     }
 
     return 0;
